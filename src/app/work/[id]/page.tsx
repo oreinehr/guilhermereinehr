b602d5311cbf2deeb52890ba247350147e0bc9b7
oreinehr <guireinehr02@gmail.com>
2025-11-17 16:57:06 -0300

    Atualizações case planeta

--- src/app/work/[id]/page.tsx
+++ src/app/work/[id]/page.tsx
@@ -1,96 +1,105 @@
 "use client";
 
-import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import Footer from "@/app/components/Footer";
 
 export default function CasePage() {
-  const galleryRef = useRef<HTMLDivElement>(null);
-  const numImages = 3;
-  const totalScrollHeight = numImages * 100; // altura em vh da seção da galeria
-
-  const { scrollYProgress } = useScroll({
-    target: galleryRef,
-    offset: ["start start", "end end"],
-  });
-
-  const x = useTransform(scrollYProgress, [0, 1], ["0%", `-${100 * (numImages - 1)}%`]);
 
   return (
     <main className="bg-black text-white">
-      {/* ------------------------- */}
-      {/* BANNER HERO - VIDEO */}
-      {/* ------------------------- */}
-     <section className="relative w-full h-screen">
-  <Image
-    src="/planeta.png"
-    alt="Planeta"
-    fill
-    className="object-cover"
-  />
-      <section className="relative w-full h-screen flex items-center justify-center">
-  <h1 className="z-10 text-[10vw] font-black tracking-tighter text-transparent stroke-text leading-none">Planeta Atlântida</h1>
-       </section>
+
+
+      <section className="relative w-full h-screen">
+        <Image
+          src="/planeta.png"
+          alt="Planeta"
+          fill
+          className="object-cover"
+        />
+
+        <section className="relative w-full h-screen flex items-center justify-center">
+          <h1 className="z-10 text-[10vw] font-black tracking-tighter text-transparent stroke-text leading-none">
+            Planeta Atlântida
+          </h1>
+        </section>
       </section>
 
+      <div className="px-6 md:px-16 py-12">
+        <h1 className="max-w-6xl text-2xl md:text-4xl font-light">
+          <span className="text-[#F2360C] font-medium">Yo,</span>{" "}
+          desenvolver o site do Planeta Atlântida foi uma das experiências mais intensas, desafiadoras e, ao mesmo tempo, gratificantes da minha trajetória como UX/UI Designer.
+          <span className="font-medium">
+            {" "}
+            O projeto chegou com uma expectativa enorme: traduzir toda a energia do maior festival do sul do Brasil em uma experiência digital fluida, moderna e imersiva.
+          </span>
+        </h1>
+      </div>
+
       {/* ------------------------- */}
-      {/* SEÇÃO DE GALERIA HORIZONTAL */}
+      {/* IMAGENS VERTICAIS GRANDES */}
       {/* ------------------------- */}
-      <section
-        ref={galleryRef}
-        className="relative bg-black"
-        style={{ height: `${totalScrollHeight}vh` }}
-      >
-        <div className="sticky top-0 h-screen flex items-center overflow-hidden px-10">
-          <motion.div style={{ x }} className="flex gap-10">
-            <Image
-              src="/gallery/gal1.png"
-              width={1200}
-              height={1500}
-              alt="gal1"
-              className="object-cover"
-            />
-            <Image
-              src="/gallery/gal2.png"
-              width={1200}
-              height={1500}
-              alt="gal2"
-              className=" object-cover"
-            />
-            <Image
-              src="/gallery/gal3.png"
-              width={1200}
-              height={1500}
-              alt="gal3"
-              className=" object-cover"
-            />
-             <Image
-              src="/gallery/gal4.png"
-              width={900}
-              height={1200}
-              alt="gal3"
-              className=" object-cover"
-            />
-          </motion.div>
-        </div>
+      <section className="w-full flex flex-col gap-20 px-6 md:px-16 py-16">
+
+        <Image
+          src="/gallery/gal1.png"
+          width={1600}
+          height={2000}
+          alt="gal1"
+          className="w-full h-auto object-cover rounded-lg"
+        />
+
+        <Image
+          src="/gallery/gal2.png"
+          width={1600}
+          height={2000}
+          alt="gal2"
+          className="w-full h-auto object-cover rounded-lg"
+        />
+
+  <div className="px-6 md:px-16 py-12">
+        <h1 className="max-w-6xl text-2xl md:text-4xl font-light">
+          <span className="text-[#F2360C] font-medium">Além</span>{" "}
+       da pressão do tempo, existia a responsabilidade de criar algo que estivesse à altura do evento. A navegação precisava ser intuitiva mesmo com grande volume de conteúdo,
+          <span className="font-medium">
+            {" "}
+        e a estética precisava conversar com um público jovem, exigente e acostumado com experiências digitais de alto impacto.
+          </span>
+        </h1>
+      </div>
+
+
+        <Image
+          src="/grid.png"
+          width={1600}
+          height={2000}
+          alt="gal3"
+          className="w-full h-auto object-cover rounded-lg"
+        />
+
+        <Image
+          src="/gallery/gal3.png"
+          width={1600}
+          height={2000}
+          alt="gal3"
+          className="w-full h-auto object-cover rounded-lg"
+        />
+
+         <div className="px-6 md:px-16 py-12">
+        <h1 className="max-w-6xl text-2xl md:text-4xl font-light">
+          <span className="text-[#F2360C] font-medium">O resultado final</span>{" "}
+não foi apenas um site funcional: foi uma experiência digital que carrega a alma do Planeta Atlântida. Um projeto grande, significativo e especial,
+          <span className="font-medium">
+            {" "}
+     que reforça o poder do design em dar vida a marcas icônicas e transformar a relação do público com o evento.
+          </span>
+        </h1>
+      </div>
+        
       </section>
 
-       <section className="relative w-full h-screen">
-  <video
-    src="/planetavideo.mp4"
-     autoPlay
-          muted
-          loop
-          playsInline
-    className="object-cover"
-  />
-  </section>
-      {/* ------------------------- */}
-      {/* CONTEÚDO NORMAL DA PÁGINA */}
-      {/* ------------------------- */}
-  
-        <Footer />
+    
+
+      <Footer />
     </main>
   );
 }
