6dfe593cf67e63e82fadb318b68dd090ba9a2357
oreinehr <guireinehr02@gmail.com>
2025-11-21 13:16:03 -0300

    Animações

--- src/app/components/Case.tsx
+++ src/app/components/Case.tsx
@@ -19,7 +19,7 @@ const wordAnimation: Variants = {
     filter: "blur(0px)",
     transition: {
       duration: 0.55,
-      ease: [0.16, 1, 0.3, 1],
+    ease: "easeOut", // válido!
     },
   },
 };
