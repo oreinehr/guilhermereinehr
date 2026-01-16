9f4a856ac347f5f29abbbceb37665a81e246c936
Guilherme Reinehr <guilhermereinehr@MacBook-Air-de-Guilherme.local>
2026-01-16 18:48:59 -0300

    fix favicon

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,36 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://reinehr.work",
+      url: "https://reinehr.work/",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
+    },
+    {
+      url: "https://www.reinehr.work/cases",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.reinehr.work/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.reinehr.work/planeta",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.reinehr.work/marina",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.reinehr.work/brazilian-footwear",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.reinehr.work/conceito",
+      lastModified: new Date(),
+    },
+      {
+      url: "https://www.reinehr.work/luciano-portfolio",
+      lastModified: new Date(),
     },
   ];
 }
